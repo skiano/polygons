78a2c7d7f68feb785b015bc933c7cae13ecc4ee8
Greg Skiano <gregoryskiano@gmail.com>
2014-05-16 00:46:38 -0400

    playing with imaginary numbers

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var d3 = require("d3")
   , output = require("./lib/outputSVG.js")
   , path = require("path")
   , outputDir = path.join(__dirname, "output")
+  , space = require("./lib/mathHelpers") 
   ;
 
 var padding = 10,
@@ -10,9 +11,50 @@ var padding = 10,
     height = 500;
 
 var points = [
-  [200, 200],
-  [760, 300]
+  [290, 300],
+  [270, 350],
+  [770, 150]
 ];
+
+
+var xDist = 11
+  , yDist = 33
+  , i
+  ;
+
+
+console.log(space.rotate([2,0], 180, [-1,0]));
+
+
+
+for(i = 1; i < 100; i+= 1){
+
+  var x = (i * xDist) % width
+    , y = Math.floor((i * xDist)/width) * yDist
+    ;
+
+  x = (Math.random() * 10) + x;
+
+  if(y > height - padding || y < padding || x > width - padding || x < padding){
+    // console.log("baddd")
+  }
+  else{
+    points.push([x,y]);
+  }
+
+}
+
+
+var i;
+for(i=0; i < 4; i+=1){
+
+  var x = (Math.random() * width - 2 * padding) + padding
+    , y = (Math.random() * height - 2 * padding) + padding
+    ;
+
+  points.push([x,y]);
+
+}
 
 var voronoi = d3.geom.voronoi()
     .clipExtent([[padding, padding], [width - padding, height - padding]]);
@@ -28,14 +70,21 @@ var polygons = voronoi(points);
 svg.selectAll("path")
     .data(voronoi(points))
   .enter().append("path")
-    .style("fill", function(d, i) { return color(i); })
+    // .style("fill", function(d, i) { return color(i); })
+    .attr("fill", "#000")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 2)
     .attr("d", function(d) { return "M" + d.join("L") + "Z"; });
 
-svg.selectAll("circle")
-    .data(points)
-  .enter().append("circle")
-    .style("fill", function(d, i) { return color(i); })
-    .attr("transform", function(d) { return "translate(" + d + ")"; })
-    .attr("r", 4.5);
+// svg.selectAll("circle")
+//     .data(points)
+//   .enter().append("circle")
+//     .style("fill", function(d, i) { return color(i); })
+//     .attr("transform", function(d) { return "translate(" + d + ")"; })
+//     .attr("r", 4.5);
 
-output(d3.select("body").html(), outputDir);
+var txt = d3.select("body").html();
+
+// console.log(txt)
+
+// output(txt, outputDir);
