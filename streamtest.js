c3063be01af8f619b4af87abe191b2a3f2cd740f
Greg Skiano <gregoryskiano@gmail.com>
2014-07-02 01:47:33 -0400

    expanding knowledge of stream stuff sort of

--- streamtest.js
+++ streamtest.js
@@ -1,5 +1,6 @@
 var Readable = require('stream').Readable;
 var Writable = require('stream').Writable;
+var Transform = require('stream').Transform;
 var util = require('util');
 
 util.inherits(Counter, Readable);
@@ -27,21 +28,37 @@ function myWriter () {
   Writable.call(this, { objectMode : true });
 }
 
-
-
 myWriter.prototype._write = function (data, encoding, done) {
   console.log(data);
   done();
 };
 
+util.inherits(MyTransformer, Transform);
+
+function MyTransformer() {
+  Transform.call(this, { objectMode : true });
+}
+
+MyTransformer.prototype._transform = function(data, encoding, done){
+  var self = this;
+  // var number = parseInt(data.toString());
+  var number = parseInt(data.toString())
+  if(number%10 === 0){
+    self.push(number);  
+  }
+  
+  done();
+}
 
 
 
-var c = new Counter
-  , w = new myWriter
+
+var c = new Counter()
+  , w = new myWriter()
+  , t = new MyTransformer()
   ;
 
-c.pipe(w)
+c.pipe(t).pipe(w)
 
 // c.on("data", function(data){
 
