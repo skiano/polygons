c40d9313c3d2a250c3d89009e67f1f7651b5dbf5
Greg Skiano <gregoryskiano@gmail.com>
2014-07-10 23:35:24 -0400

    playing with clips and ppunches

--- index.js
+++ index.js
@@ -6,23 +6,96 @@ var _ = require("underscore")
   , path = require("path")
   , outputDir = path.join(__dirname, "output", "day-2")
   , g2d = require("./lib/utils/2dHelpers")
-  , shape = require("./lib/shapes/")
-  , frame = require("./lib/frame/")
+  , frame = require("./lib/frame")
+  , circle = require("./lib/shapes/circle")
+  , collectStream =  require("./lib/utils/streamHelpers").collectStream;
   ;
 
-var picture = frame(200, 200, 10);
+var picture = frame(230, 230, 0); // 0 margin
 
-shape.circle([50,50],10).outline(10, function(dot){
-  picture.addDot(dot);
+var circleA = circle([115,115], 90);
+var circleB = circle([100,120], 60);
+var circleC = circle([60,125], 40);
+var circleD = circle([130,55], 100);
+var circleE = circle([200,115], 120);
+var points = collectStream();
+
+circleA.outlineStream()
+  .pipe(circleB.clipStream())
+  .pipe(points);
+
+circleC.outlineStream()
+  .pipe(circleA.clipStream())
+  .pipe(circleB.clipStream())
+  .pipe(points);
+
+circleB.outlineStream()
+  .pipe(circleA.clipStream())
+  .pipe(points);
+
+circleA.outlineStream()
+  .pipe(points);
+
+circleD.outlineStream()
+  .pipe(circleA.punchStream())
+  .pipe(points);
+
+circleD.outlineStream()
+  .pipe(circleB.clipStream())
+  .pipe(circleC.clipStream())
+  .pipe(points);
+
+circleE.outlineStream()
+  .pipe(circleB.punchStream())
+  .pipe(points);
+
+
+
+
+
+for(var i = 1; i < 30; i+=1){
+
+  circle([200,115 + i*10], 120).outlineStream()
+    .pipe(circleE.clipStream())
+    .pipe(circleB.punchStream())
+    .pipe(points);
+}
+
+for(var i = 1; i < 50; i+=1){
+
+  circle([100,-100 + i*20], 130).outlineStream()
+    .pipe(circleA.punchStream())
+    .pipe(circleD.punchStream())
+    .pipe(points);
+}
+
+for(var i = 1; i < 50; i+=1){
+
+  circle([100, 100 + i*10], 130).outlineStream()
+    .pipe(circleA.clipStream())
+    .pipe(circleB.punchStream())
+    .pipe(points);
+}
+
+points.on("finish", function(){
+  picture.addDots(this.data);
+  // picture.preview();
+  picture.export(outputDir);
 });
 
-shape.circle([150,150],100).outline(20, function(dot){
-  picture.addDot(dot);
-});
 
-shape.circle([250,250],100).outline(20, function(dot){
-  picture.addDot(dot);
-});
 
-picture.preview();
-picture.export(outputDir);
+// shape.circle([50,50],10).outline(10, function(dot){
+//   picture.addDot(dot);
+// });
+
+// shape.circle([150,150],100).outline(20, function(dot){
+//   picture.addDot(dot);
+// });
+
+// shape.circle([250,250],100).outline(20, function(dot){
+//   picture.addDot(dot);
+// });
+
+// picture.preview();
+// picture.export(outputDir);
